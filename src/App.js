a07bb138b44039ae3c94e83976165d3e9a0fe397
lakshan969Github <lakshand969@gmail.com>
2022-12-19 21:56:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Home from './components/Home';
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>React</h1>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return(
+        <div>
+            <h1>This is the Home</h1>
+        </div>
+    );
+
+}
+
+export default Home;
