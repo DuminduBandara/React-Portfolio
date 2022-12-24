80cf0fa8263f073dad6292ac01a4d30fefcd9b41
lakshan969Github <lakshand969@gmail.com>
2022-12-24 12:15:55 +0530

    updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -21,7 +21,35 @@ const Project = () => {
             initial= "hidden"
             animate= "visible"
         >
-            <h1>This is the Project</h1>
+            <motion.div 
+                initial={{
+                    opacity: 0,
+                    y: 300
+                }}
+                animate= {{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    type: "spring",
+                    stiffness: 100,
+                    dumping:50
+                }}
+                className="my-6">
+                <h1 className="text-[#0000005b] text-[3.5rem] md:text-[5rem] lg:text-[13rem] text-center font-header">Projects.</h1>
+            </motion.div>
+            <div>
+                <div>
+                    <div>
+                        <img src=""/>
+                    </div>
+                    <div>
+                        <h1></h1>
+                        <p></p>
+                    </div>
+                    <a href="/"/>
+                </div>
+            </div>
         </motion.div>
      );
 }
